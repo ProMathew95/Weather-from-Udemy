e564d842bf8eedf6cf9174ff1cf2043ba1ee88e2
ProMathew95 <mathew95lj@gmail.com>
2018-01-03 05:39:26 +0200

    Create  Weather Structures.swift

---
+++ Weather Structures.swift
@@ -0,0 +1,48 @@
+//
+//  Weather Structures.swift
+//  Simple Weather App
+//
+//  Created by mathew on 2017-10-01.
+//  
+//
+
+import Foundation
+
+struct WeatherData : Codable {
+    let list : [WeatherDataPoint]
+    let city : City
+}
+
+struct City : Codable {
+    let name : String
+    let country : String
+}
+
+struct WeatherDataPoint : Codable {
+    let main : Temperatures
+    let dt_txt : String
+    let weather : [WeatherIconData]
+}
+
+struct Temperatures : Codable {
+    let temp : Double
+    let temp_min : Double
+    let temp_max : Double
+}
+
+struct WeatherIconData : Codable {
+    let icon : String
+}
+
+struct WeatherByDay {
+    let date: String
+    let averageTemp: Double
+    let averageMinTemp: Double
+    let averageMaxTemp: Double
+    let iconString: String
+}
+
+enum Day {
+    case today
+    case tomorrow
+}
